d6f9176c5b0cf4610e9889e9ddae29bb4911ca18
Manu <ominuka.mauro@gmail.com>
2022-10-07 15:21:21 -0300

    Feat: Add new server for Socket and create a new connection

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 import express from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
+
 const PORT = 5000
+
 const app = express()
+const httpServer = http.createServer(app)
 
-app.get('/', (req,res)=>{})
+const io = new Server(httpServer, { cors: ['http://localhost:3000'] })
 
-app.listen(PORT,()=>{ console.log('Running in: ', PORT)})
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/index.html')
+})
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('send-message', (data) => {
+    console.log(data)
+    socket.broadcast.emit('message-from-server', data)
+  })
+})
+
+httpServer.listen(PORT, () => {
+  console.log('Running in: ', PORT)
+})
