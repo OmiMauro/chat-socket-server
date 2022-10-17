d349312c48cdf3fe267c5594c783f0bf56477353
Manu <ominuka.mauro@gmail.com>
2022-10-17 15:05:22 -0300

    Store in db a newRoom

--- socket/controllers/RoomController.js
+++ socket/controllers/RoomController.js
@@ -1,10 +1,13 @@
 import BaseController from './BaseController.js'
-
+import Room from '../../models/Room.js'
 export default class RoomController extends BaseController {
   joinRoom = ({ roomId }) => {
     this.socket.join(roomId)
   }
-  newRoomCreated = ({ roomId }) => {
+  newRoomCreated = async ({ roomId }) => {
+    const room = new Room({ name: 'Test', roomId })
+    const document = await room.save()
+    console.log(document)
     this.socket.broadcast.emit('new-room-created', { roomId })
   }
 }
