b87ef33c0a3f50f888c25e6cc5a5b62878dcfe6b
Manu <ominuka.mauro@gmail.com>
2022-10-17 19:43:08 -0300

    Added property for userId

--- models/Room.js
+++ models/Room.js
@@ -3,6 +3,7 @@ import mongoose, { Schema } from 'mongoose'
 const RoomSchema = new Schema({
   name: String,
   roomId: String,
+  userId: String,
 })
 
 const Room = mongoose.model('Room', RoomSchema)
