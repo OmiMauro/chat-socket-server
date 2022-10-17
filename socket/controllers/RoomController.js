a90d9d35e15c0f84ddd1c275d69ef477ae852789
Manu <ominuka.mauro@gmail.com>
2022-10-17 20:26:41 -0300

    Add method for roomRemoved

--- socket/controllers/RoomController.js
+++ socket/controllers/RoomController.js
@@ -4,10 +4,14 @@ export default class RoomController extends BaseController {
   joinRoom = ({ roomId }) => {
     this.socket.join(roomId)
   }
-  newRoomCreated = async ({ roomId }) => {
-    const room = new Room({ name: 'Test', roomId })
-    const document = await room.save()
-    console.log(document)
-    this.socket.broadcast.emit('new-room-created', { roomId })
+  newRoomCreated = async ({ roomId, userId }) => {
+    const room = new Room({ name: 'Test', roomId, userId })
+    await room.save()
+    console.log(room)
+    this.socket.emit('new-room-created', { room })
+  }
+  roomRemoved = async ({ roomId }) => {
+    await Room.deleteOne({ roomId })
+    this.socket.emit('room-removed', { roomId })
   }
 }
