477b9e5721f72942cc4e96f6868543420b0967d5
Manu <ominuka.mauro@gmail.com>
2022-10-17 10:51:01 -0300

    Manage sockets with cleanCode

---
+++ socket/sockets.js
@@ -0,0 +1,20 @@
+import TypingController from './controllers/TypingController.js'
+import MessageController from './controllers/MessageController.js'
+import RoomController from './controllers/RoomController.js'
+
+const sockets = (socket) => {
+  const typingController = new TypingController(socket)
+  const messageController = new MessageController(socket)
+  const roomController = new RoomController(socket)
+
+  socket.on('send-message', messageController.sendMessage)
+
+  socket.on('typing-started', typingController.typingStarted)
+  socket.on('typing-end', typingController.typingStoped)
+  socket.on('join-room', roomController.joinRoom)
+
+  socket.on('disconnect', (socket) => {
+    console.log('user disconnected')
+  })
+}
+export default sockets
