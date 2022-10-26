b4050ed8789ab8443b282d53b71a3bec582a4179
Manu <ominuka.mauro@gmail.com>
2022-10-26 14:35:38 -0300

    set passport and express-session

--- index.js
+++ index.js
@@ -3,20 +3,43 @@ import http from 'http'
 import { Server } from 'socket.io'
 import cors from 'cors'
 import sockets from './socket/sockets.js'
-import { db } from './db.js'
-import router from './api/routes.js'
-const PORT = 5000
-
+import { db } from './db/db.js'
+import router from './routes/index.js'
+import session from 'express-session'
+import passport from 'passport'
+import 'dotenv/config'
+const PORT = process.env.PORT || 5000
 const app = express()
 const httpServer = http.createServer(app)
 db()
-app.use(cors())
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    cookie: { secure: true },
+    resave: true,
+    name: 'session',
+    keys: ['oauth'],
+    maxAge: 24 * 60 * 60 * 100,
+  })
+)
+app.use(
+  cors({
+    //set this in enviroment var
+    origin: process.env.URL_ORIGIN,
+    credentials: true,
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use('/api/v1', router)
+
 const io = new Server(httpServer, {
   cors: {
-    origin: ['http://localhost:3000'],
+    origin: [process.env.URL_ORIGIN],
   },
 })
-app.use('/', router)
 io.on('connection', sockets)
 
 httpServer.listen(PORT, () => {
